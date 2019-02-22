854ab1143383b0321beac1295f1cdbc6f3f831ca
Daniel Ors <do7811@gmail.com>
2019-02-22 14:11:05 -0500

    working on handlers for bookmarks

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 // const { bookmarks } = require('./store');
+const uuid = require('uuid/v4');
 const { NODE_ENV } = require('./config');
 
 const app = express();
@@ -17,31 +18,35 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
+// app.use(function validateBearerToken(req, res, next) {
+//   const apiToken = process.env.API_TOKEN;
+//   const authToken = req.get('Authorization');
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    // logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  // move to the next middleware
-  next();
-});
+//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//     // logger.error(`Unauthorized request to path: ${req.path}`);
+//     return res.status(401).json({ error: 'Unauthorized request' });
+//   }
+//   // move to the next middleware
+//   next();
+// });
 
 const bookmarks = [
   {
     id:1,
     title:'google',
-    description:'search engine'
+    description:'search engine',
+    link:'https://google.com'
   }
 ];
 
+// /bookmarks/ 
+//GET bookmarks
 app.get('/bookmarks', (req, res) => {
   res.send(bookmarks);
 });
 
 
+// placeholder response for / location
 app.get('/', (req,res) => {
   res.send('Hello, world!');
 });
